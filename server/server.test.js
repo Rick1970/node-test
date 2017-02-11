ed1e333346ee1a4c0f6efe0bd23e6c5c72077f9b
Rick Thornbrugh <Rick Thornbrugh>
2017-02-10 21:56:21 -0800

    working with spies for testing functions.

--- server/server.test.js
+++ server/server.test.js
@@ -4,28 +4,31 @@ const expect = require('expect');
 var app = require('./server').app;
 
 describe('Server', () => {
-  it('should return Hello World! response', (done) => {
-    request(app)
-    .get('/')
-    .expect(404)
-    .expect((res) => {
-      expect(res.body).toInclude({
-        error: 'Page not found.'
-      });
-    })
-    .end(done);
+  describe('GET /', () => {
+    it('should return Hello World! response', (done) => {
+      request(app)
+      .get('/')
+      .expect(404)
+      .expect((res) => {
+        expect(res.body).toInclude({
+          error: 'Page not found.'
+        });
+      })
+      .end(done);
+    });
   });
-
-  it('should return user object name Rick', (done) => {
-    request(app)
-    .get('/users')
-    .expect(200)
-    .expect((res) => {
-      expect(res.body).toInclude({
-        name: 'Rick',
-        age: 46
-      });
-    })
-    .end(done);
+  describe('GET /users', () => {
+    it('should return user object name Rick', (done) => {
+      request(app)
+      .get('/users')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body).toInclude({
+          name: 'Rick',
+          age: 46
+        });
+      })
+      .end(done);
+    });
   });
 });
---
+++ spies/app.js
@@ -0,0 +1,7 @@
+var db = require ('./db.js');
+
+module.exports.handleSignup = (email, password) => {
+  //check if email exist
+  db.saveUser({email, password});
+  //send the welcome email
+};
